7ebb45c245069addf6df809fa3ee5a13560dd0dc
Anh Tran <anhminhtran268@gmail.com>
2025-05-16 01:13:45 -0400

    cleanup

--- src/app/server/create-hex-action.ts
+++ src/app/server/create-hex-action.ts
@@ -1,18 +1,14 @@
 import { useState } from "react";
 import seedrandom from 'seedrandom';
 
-// Function to generate a random hex color
-export function generateHexColor(salt : number) {
-  const hexDigits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
-  let color = "#";
+  // Function to generate a random hex color
+  function generateHexColor() {
+    const hexDigits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      const randomIndex = Math.floor(Math.random() * hexDigits.length);
+      color += hexDigits[randomIndex];
+    }
+    return color;
+  };
 
-  for (let i = 0; i < 6; i++) {
-
-    // creates random index, using score as seed
-    const randomIndex = Math.floor(seedrandom(salt.toString())() * hexDigits.length);
-
-    // appends new hexdigit with new rng index to color
-    color += hexDigits[randomIndex];
-  }
-  return color;
-};
