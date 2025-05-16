87e7c9714497b919279abfa4393ed3ac274340cb
ellioats <emwcurrie@gmail.com>
2025-05-16 03:59:38 -0400

    added functionality to query blockchain for total number of users that interact with nft based on issuer #impl

---
+++ src/app/server/query-rpc-action.ts
@@ -0,0 +1,34 @@
+import Server from "@stellar/stellar-sdk";
+
+const server = new Server("https://horizon.stellar.org");
+
+const assetCode = "";
+const issuer = "REPLACE WITH ACTUAL ISSUER KEY";
+
+export function fetchTransactionList() {
+
+
+
+
+
+    
+
+}
+
+// uses trustline count to get total number of leaderboard participants
+export async function getTrustLineAccounts() {
+
+    const response = await server.assets()
+        .forCode(assetCode)
+        .forIssuer(issuer)
+        .call();
+
+    const assetRecord = response[0];
+
+    if (!assetRecord) {
+        throw new Error("Asset not found on network...");
+    }
+
+    return assetRecord.num_accounts;
+
+}
