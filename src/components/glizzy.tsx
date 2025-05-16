df2e9cc151749e0d333e36a4a21ab3283b57915a
ellioats <emwcurrie@gmail.com>
2025-05-15 23:57:33 -0400

    various changes

--- src/components/glizzy.tsx
+++ src/components/glizzy.tsx
@@ -1,28 +1,32 @@
 import { useEffect, useState } from "react";
 
 export default function Page() {
-        const [position, setPosition] = useState({ x: 0, y: 0 });
 
-        useEffect(() => {
-            const handleMouseMove = (event: MouseEvent) => {
-                setPosition({ x: event.clientX, y: event.clientY });
-            };
+    const [position, setPosition] = useState({ x: 0, y: 0 });    // counts for the times the user crossed each line
+    const [crossedUpperLine, setCrossedUpperLine] = useState(false);
+    const [crossedLowerLine, setCrossedLowerLine] = useState(false);
 
-            window.addEventListener('mousemove', handleMouseMove);
-
-            // Cleanup on unmount
-            return () => {
-                window.removeEventListener('mousemove', handleMouseMove);
-            };
-        }, []);
-
-    // counts for the times the user crossed each line
-    const [crossedUpperLine, setCrossedUpperLine] = useState(Boolean);
-    const [crossedLowerLine, setCrossedLowerLine] = useState(Boolean);
+    const [points, setPoints] = useState(0);
 
     // represents each line
     const TARGET_Y_UPPER = 200;
     const TARGET_Y_LOWER = 800;
+
+    // when mouse moves
+    useEffect(() => {
+        const handleMouseMove = (event: MouseEvent) => {
+            setPosition({ x: event.clientX, y: event.clientY });
+        };
+
+        window.addEventListener('mousemove', handleMouseMove);
+
+        // Cleanup on unmount
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+        };
+    }, []);
+
+
 
 
     return (
