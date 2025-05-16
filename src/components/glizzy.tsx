7531ddb425b0b4559e6b4a676fb6faae18938354
Anh Tran <anhminhtran268@gmail.com>
2025-05-15 23:43:25 -0400

    gbfgfgfgf

--- src/components/glizzy.tsx
+++ src/components/glizzy.tsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
 
 export default function Page() {
+        const [position, setPosition] = useState({ x: 0, y: 0 });
 
+        useEffect(() => {
+            const handleMouseMove = (event: MouseEvent) => {
+                setPosition({ x: event.clientX, y: event.clientY });
+            };
+
+            window.addEventListener('mousemove', handleMouseMove);
+
+            // Cleanup on unmount
+            return () => {
+                window.removeEventListener('mousemove', handleMouseMove);
+            };
+        }, []);
     return (
     <>
     
     </>
-);
-}
+)}
