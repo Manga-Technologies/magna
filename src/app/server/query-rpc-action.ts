8f8a620a96a70b264878e93c62739f700d37ef10
ellioats <emwcurrie@gmail.com>
2025-05-16 07:17:35 -0400

    random af file

--- src/app/server/query-rpc-action.ts
+++ src/app/server/query-rpc-action.ts
@@ -1,31 +1,5 @@
-import Server from "@stellar/stellar-sdk";
+// define keys here
 
-const server = new Server("https://horizon.stellar.org");
+// call helper functions here
 
-const assetCode = "";
-const issuer = "REPLACE WITH ACTUAL ISSUER KEY";
-
-export function fetchTransactionList() {
-
-
-
-
-
-    
-
-}
-
-// uses trustline count to get total number of leaderboard participants
-// queries horizon endpoint
-export async function getTrustLineAccounts() {
-
-    const response = await fetch(`/api/fetch-tl-acc?assetCode=${assetCode}&issuer=${issuer}`);
-    const data = await response.json()
-    const assetRecord = data._embedded?.records?.[0];
-
-    if (!assetRecord) {
-        throw new Error("Asset not found on network...");
-    }
-
-    return assetRecord.num_accounts;
-}
+// yo why tf here
