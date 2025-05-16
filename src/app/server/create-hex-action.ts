9418f332667568fbc441e68d5a9a12e30fa2c483
ellioats <emwcurrie@gmail.com>
2025-05-16 00:17:55 -0400

    added create-hex-actions

---
+++ src/app/server/create-hex-action.ts
@@ -0,0 +1,11 @@
+import seedrandom from 'seedrandom';
+
+export function createHex(score : number) {
+    const HEX_DIGITS = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
+
+    let hexCode = "#";
+
+    
+
+
+}
