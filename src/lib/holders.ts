641d99b69df4e90f87b03d1f1a082bf80da019a3
ellioats <emwcurrie@gmail.com>
2025-05-16 06:10:31 -0400

    added documentation

--- src/lib/holders.ts
+++ src/lib/holders.ts
@@ -2,6 +2,7 @@ import { AssetHolder } from "../../types/asset-holder";
 
 const STELLAR_EXPERT_API = 'https://api.stellar.expert/explorer/public';
 
+// gets all assets issued by us
 export async function getIssuedAssets(issuer: string) {
   const res = await fetch(`${STELLAR_EXPERT_API}/account/${issuer}/assets`);
   if (!res.ok) throw new Error('Failed to fetch assets');
@@ -9,6 +10,7 @@ export async function getIssuedAssets(issuer: string) {
   return data._embedded.records;
 }
 
+// gets holders of assets, specifically wallet address and balance
 export async function getAssetHolders(assetCode: string, issuer: string) {
   const res = await fetch(`${STELLAR_EXPERT_API}/asset/${assetCode}-${issuer}/holders`);
   if (!res.ok) throw new Error(`Failed to fetch holders for ${assetCode}`);
@@ -16,6 +18,7 @@ export async function getAssetHolders(assetCode: string, issuer: string) {
   return data._embedded.records;
 }
 
+// master function
 export async function getAllAssetHolders(issuer: string) {
   const assets = await getIssuedAssets(issuer);
   const holdersMap: Record<string, { account: string; balance: string }[]> = {};
