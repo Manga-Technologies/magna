325c1fe0256a5744d80cdc2cc66586205b7fe846
ellioats <emwcurrie@gmail.com>
2025-05-15 23:41:50 -0400

    added use states and target values for each line mouse has to cross #impl

--- src/components/glizzy.tsx
+++ src/components/glizzy.tsx
@@ -1,5 +1,15 @@
+import { useState } from "react";
 
 export default function Page() {
+
+    // counts for the times the user crossed each line
+    const [crossedUpperLine, setCrossedUpperLine] = useState(Boolean);
+    const [crossedLowerLine, setCrossedLowerLine] = useState(Boolean);
+
+    // represents each line
+    const TARGET_Y_UPPER = 200;
+    const TARGET_Y_LOWER = 800;
+
 
     return (
     <>
